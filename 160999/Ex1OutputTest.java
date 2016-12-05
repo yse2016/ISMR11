4af89730583a64113c38b5d51cd712c49b245657
{yoshinotaichi} <{yoshinotaichi@gmail.com}>
2016-12-05 14:05:05 +0900

    files handled.

--- 160999/Ex1OutputTest.java
+++
@@ -1,9 +0,0 @@
-public class Ex1OutputTest {
-  public static void main(String[] args) {
-    String data1 = args[0];
-    String data2 = args[1];
-
-    System.out.println(data1);
-    System.err.println(data2);
-  }
-}
